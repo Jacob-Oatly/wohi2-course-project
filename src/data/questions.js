653c0a3cbc653b89cee50c2d33c41298b1fa3d0c
Jaakko <kauranen.jaakko@hotmail.com>
2026-04-12 18:41:57 +0300

    Creating posts API

---
+++ src/data/questions.js
@@ -0,0 +1,15 @@
+const questions = [
+    {
+        id: 1,
+        questionTitle: "Unity or Godot",
+        answer: "Godot",
+        keywords: ["game engine"]
+    },
+    {
+        id: 2,
+        questionTitle: "Chrome or Firefox",
+        answer: "Firefox",
+        keywords: ["browser"]
+    }
+];
+module.exports = questions;
